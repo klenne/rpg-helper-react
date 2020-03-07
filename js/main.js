4e2985b440b847d5c0d517f75e057eae7a3d0e1d
Guilherme Klesse de Miranda <guiklesse@gmail.com>
2020-03-07 13:02:29 -0300

    versão melhorada

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 window.onload = function () {
-    $("#cards-party").append(getCookie("partySalva"))
-   
 
+    $("#cards-party").append(localStorage.getItem("tagsParty"))
+    $("#cards-enemies").append(localStorage.getItem("tagsEnemies"))
+    //localStorage.removeItem("tagsParty")
 }
 
 
@@ -17,27 +18,66 @@ $("#btnNewParty").click(function () {
     <div class="card-in">
 
         <div class="title-card">
-            <label>Jogador:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+            <label>Jogador:</label><span spellcheck="false" contenteditable class="inputs"></span>
             <i onclick="excluir('${numeroCard}')" class="fas fa-trash"></i>
             <i onclick="salvar()" class="fas fa-save"></i>
         </div>
-        <div class="center-image-char">
-            <img class="image-char" id="imagem${numeroCard}" src="media/img/barb.png">
+        <div class="center-image-char" onclick="carregarImagem('imagem${numeroCard}')">
+            <img class="image-char" id="imagem${numeroCard}"
+                src="media/img/57-572389_twenty-sided-dice-20-sided-dice-png-clipart.png">
         </div>
         <div class="label-n-input">
-            <label>Nome-Char:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+            <label>Nome-Char:</label><span spellcheck="false" contenteditable class="inputs"></span>
         </div>
         <div class="label-n-input">
-            <label>Vida:</label><span spellcheck="false" contenteditable class="inputs"  onkeypress="vida(event,${numeroCard})" id="vida${numeroCard}" ></span> 
+            <label>Classe:</label><span spellcheck="false" contenteditable class="inputs"></span>
         </div>
         <div class="label-n-input">
-            <label>Buffs:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+            <label>Armour:</label><span spellcheck="false" contenteditable class="inputs"></span>
+        </div>
+        <div class="abilities">
+            <div class="abilitie-inside border-abilitie">
+                <span>STR</span>
+                <span spellcheck="false" contenteditable class="inputs-abilities"></span>
+            </div>
+            <div class="abilitie-inside border-abilitie">
+                <span>DEX</span>
+                <span spellcheck="false" contenteditable class="inputs-abilities"></span>
+            </div>
+            <div class="abilitie-inside border-abilitie">
+                <span>CON</span>
+                <span spellcheck="false" contenteditable class="inputs-abilities"></span>
+            </div>
+            <div class="abilitie-inside border-abilitie">
+                <span>INT</span>
+                <span spellcheck="false border-abilitie" contenteditable class="inputs-abilities"></span>
+            </div>
+            <div class="abilitie-inside border-abilitie">
+                <span>WIS</span>
+                <span spellcheck="false" contenteditable class="inputs-abilities"></span>
+            </div>
+            <div class="abilitie-inside">
+                <span>CHA</span>
+                <span spellcheck="false" contenteditable class="inputs-abilities"></span>
+            </div>
+
+        </div>
+
+        <div class="label-n-input">
+            <label>Vida:</label><span spellcheck="false" contenteditable class="inputs"
+                onkeypress="vida(event,'${numeroCard}')" id="vida${numeroCard}"></span>
         </div>
         <div class="label-n-input">
-            <label>Debufs:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+            <label>Saving-Throws:</label><span spellcheck="false" contenteditable class="inputs input-menor"></span>
+        </div>
+        <div class="label-n-input">
+            <label>Buffs:</label><span spellcheck="false" contenteditable class="inputs"></span>
+        </div>
+        <div class="label-n-input">
+            <label>Debufs:</label><span spellcheck="false" contenteditable class="inputs"></span>
         </div>
     </div>
-     </div>
+</div>
 
 
    `
@@ -47,14 +87,38 @@ $("#btnNewParty").click(function () {
 
 });
 
+let imagemId
+function carregarImagem(idImagem) {
+    imagemId = idImagem
+    $("#modalParty").css("display", "flex")
+    let pos=$("#modalParty").position().top-1000
+    $('html, body').animate({ 'scrollTop' :pos  }, 1);
+
+}
+
+$("#fechareModal").click(function () {
+    $("#modalParty").css("display", "none")
+
+});
+
+$("#AddImg").click(function () {
+    let img = document.getElementById(imagemId);
+    let url = $("#urlImage").val()
+    img.src = url;
+    $("#urlImage").val("")
+    
+    $("#modalParty").css("display", "none")
+});
 
 function salvar() {
 
 
     let html = $("#cards-party").html()
+    localStorage.removeItem("tagsPaty")
+    localStorage.setItem("tagsParty", html)
 
-    setCookie("partySalva", html.toString())
-   
+
+
 }
 function excluir(cardTarget) {
 
@@ -62,47 +126,8 @@ function excluir(cardTarget) {
     $(target).remove()
     salvar()
 }
-function vida(e, numeroCard) {
-    if (e.keyCode == 13) {
-        e.preventDefault();
-        let target = "#vida" + numeroCard
-        let valor = $(target).html()
-
-        if (valor.includes("+")) {
-            let splitSoma = valor.split("+")
-            let soma = parseInt(splitSoma[0]) + parseInt(splitSoma[1])
-            $(target).html(soma.toString());
 
 
-
-        } else
-            if (valor.includes("-")) {
-                let splitmenos = valor.split("-")
-                let menos = parseInt(splitmenos[0]) - parseInt(splitmenos[1])
-                $(target).html(menos.toString());
-
-
-
-            } else if (valor.includes("*")) {
-                let splitMult = valor.split("*")
-                let mult = parseInt(splitMult[0]) * parseInt(splitMult[1])
-                $(target).html(mult.toString());
-
-
-
-
-
-            } else if (valor.includes("/")) {
-                let splitDiv = valor.split("/")
-                let div = parseInt(splitDiv[0]) / parseInt(splitDiv[1])
-                $(target).html(div.toString());
-
-
-
-            }
-        salvar()
-    }
-}
 
 
 $("#btnExportParty").click(function () {
@@ -132,7 +157,115 @@ $("#btnImportParty").click(function () {
                 let fileReader = new FileReader()
                 fileReader.onload = function (e) {
                     let fileContents = document.getElementById('cards-party')
-                    fileContents.innerHTML = fileReader.result
+                    let htmlAtual = $('#cards-party').html()
+                    fileContents.innerHTML = htmlAtual + fileReader.result
+                }
+                fileReader.readAsText(fileTobeRead)
+            }
+            else {
+                alert("Por favor selecione arquivo texto")
+            }
+
+        }, false)
+    }
+    else {
+        alert("Arquivo(s) não suportado(s)")
+    }
+});
+
+
+
+
+$("#btnNewEnemie").click(function () {
+
+    let numeroCard = uuid()
+
+    let template = `
+ 
+
+    <div id="card${numeroCard}" class="card-out cardEnemie">
+    <div class="card-in">
+
+        <div class="title-card">
+            <span spellcheck="false" contenteditable class="inputs ,
+            "></span> 
+            <i onclick="excluirEnemie('${numeroCard}')" class="fas fa-trash"></i>
+            <i onclick="salvarEnemies()" class="fas fa-save"></i>
+        </div>
+        <div class="center-image-char" onclick="carregarImagem('imagem${numeroCard}')">
+            <img class="image-char" id="imagem${numeroCard}" src="media/img/beholder.png">
+        </div>
+        <div class="label-n-input">
+            <label>Vida:</label><span spellcheck="false" contenteditable class="inputs"  onkeypress="vida(event,'${numeroCard}')" id="vida${numeroCard}" ></span> 
+        </div>
+        <div class="label-n-input">
+            <label>Buffs:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+        </div>
+        <div class="label-n-input">
+            <label>Debufs:</label><span spellcheck="false" contenteditable class="inputs"></span> 
+        </div>
+    </div>
+     </div>
+
+
+   `
+
+    $("#cards-enemies").append(template)
+    salvarEnemies()
+
+});
+
+
+
+
+
+function salvarEnemies() {
+
+
+    let html = $("#cards-enemies").html()
+    localStorage.removeItem("tagsEnemies")
+    localStorage.setItem("tagsEnemies", html)
+
+
+
+}
+
+function excluirEnemie(cardTarget) {
+
+    let target = "#card" + cardTarget
+    $(target).remove()
+    salvarEnemies()
+}
+
+
+$("#btnExportEnemies").click(function () {
+
+    let html = $("#cards-enemies").html()
+    let blob = new Blob([html], { type: "text/plain;charset=utf-8" })
+    saveAs(blob, "ExportedEnemies.txt")
+
+
+});
+
+$("#btnImportEnemies").click(function () {
+
+
+    //Check the support for the File API support
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        let fileSelected = document.getElementById('file_uploadEnemie')
+        fileSelected.addEventListener('change', function (e) {
+            //Set the extension for the file
+            let fileExtension = /text.*/
+            //Get the file object
+            let fileTobeRead = fileSelected.files[0]
+            //Check of the extension match
+            if (fileTobeRead.type.match(fileExtension)) {
+                //Initialize the FileReader object to read the 2file
+                let fileReader = new FileReader()
+                fileReader.onload = function (e) {
+                    let fileContents = document.getElementById('cards-enemies')
+                    let htmlAtual = $('#cards-enemies').html()
+                    fileContents.innerHTML = htmlAtual + fileReader.result
                 }
                 fileReader.readAsText(fileTobeRead)
             }
@@ -181,14 +314,4 @@ function uuid() {
     return 'xxxxxxxx-xxxx-4xxx-8xxx-xxxxxxxxxxxx'.replace(/x/g, randomDigit)
 }
 
-function setCookie(name, value) {  //função universal para criar cookie
-    $.Storage.set(name, value);
-}
 
-function eraseCookie(name) {
-    document.cookie = name+'=; Max-Age=-99999999;';  
-}
-
-function getCookie(name) {
-return $.Storage.get(name);
-  }
